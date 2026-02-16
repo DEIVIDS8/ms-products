4465839e35f8b97bcd64c1634321fbe45520c3fc
Erick Salas <erick.salas@ids.com.mx>
2026-02-16 17:06:56 -0600

    validacion

--- ms-products/src/main/java/com/entity/ProductEntity.java
+++ ms-products/src/main/java/com/entity/ProductEntity.java
@@ -10,26 +10,26 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ProductEntity {
 
-    @Null(message = "{}")
+    @Null(message = "{product.id.null}")
     private Long id;
 
-    @NotBlank(message = "{}")
-    @Size(min = 3, max = 100, message = "{}")
+    @NotBlank(message = "{product.name.notblank}")
+    @Size(min = 3, max = 100, message = "{product.name.size}")
     private String name;
 
-    @NotBlank(message = "{}")
-    @Pattern(regexp = "^PROD-\\d{4}$", message = "{}")
+    @NotBlank(message = "{product.code.notblank}")
+    @Pattern(regexp = "^PROD-\\d{4}$", message = "{product.code.pattern}")
     private String code;
 
-    @NotNull(message = "{}")
-    @DecimalMin(value = "0.01", message = "{}")
+    @NotNull(message = "{product.price.notnull}")
+    @DecimalMin(value = "0.01", message = "{product.price.min}")
     private Float price;
 
-    @Min(value = 0, message = "{}")
+    @Min(value = 0, message = "{product.stock.min}")
     private Integer stock;
 
-
-    @NotBlank(message = "{}")
-    @Size(min = 3, max = 50, message = "{}")
+    @NotBlank(message = "{product.category.notblank}")
+    @Size(min = 3, max = 50, message = "{product.category.size}")
     private String category;
+}
 }
